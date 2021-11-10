1f20c2296711f7f545f61dba55db1460216f4185
Frankelin Pichardo <kelinp25@gmail.com>
2021-11-10 17:10:28 -0500

    testing

--- firstroutesandcontrollers/app/controllers/users_controller.rb
+++ firstroutesandcontrollers/app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all
+        render json: @users
+    end
 
+    # helllllllllllppppppppppppooooooooooooo
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def edit
 end
